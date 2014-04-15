67435119926f478ce55915f26d7e514ae7dcb88e
DragosGeornoiu <dragosgeornoiu@gmail.com>
2014-04-15 13:16:24 -0700

    A minor update in code and submit of ppt.

--- BST/Geometric Applications/Rectangle Intersection by Geornoiu Dragos/functions.cpp
+++ BST/Geometric Applications/Rectangle Intersection by Geornoiu Dragos/functions.cpp
@@ -1,31 +1,30 @@
 #include "header.h"
 #include <time.h>
+#include <random>
 #include <vector>
 #include<fstream>
+#include<string>
 
-#define N 1000
+#define N 16000
 
-void create()
+int create(char* file)
 {
 	int x,y,l,h;
 	FILE *fp;
 	srand (time(NULL));
 	std::vector<int> input;
-	fp = fopen("figuri.txt","w");
+	fp = fopen(file,"w");
 	if( fp == NULL )
-	{
-      printf("Error while opening the file.\n");
-      exit(0);
-	}
+		return 0;
 
 	int i=0,ok=0;
 	while(i<N)
 	{
-		
-		x = rand() % N*10 + 1;		
-		y = rand() % N*10 + 1;
-		h = rand() % N*10 + 1;
-		l = rand() % N*10 + 1;
+
+		x = rand() % (N*100) + 1;
+		y = rand() % (N*100) + 1;
+		h = rand() % (N*100) + 1;
+		l = rand() % (N*100) + 1;
 
 		ok = 1;
 
@@ -40,79 +39,78 @@ void create()
 		{
 			input.push_back(x);
 			input.push_back(x+l);
-			
+
 			fprintf(fp,"%d %d %d %d\n",x,y,l,h);
 			i++;
 		}
-		
+
 
 	}
-	
-	fclose(fp);
 
+	fclose(fp);
+	return 1;
 }
 
 NodArb *add(MinHeap temp, NodArb *root)
 {
-	NodArb *nod1, *nod2, *nod3; 
-	
-	nod1 = new NodArb; 
+	NodArb *nod1, *nod2, *nod3;
+
+	nod1 = new NodArb;
 	nod1->info.x = temp.x;
 	nod1->info.y = temp.y;
 	nod1->info.lenght = temp.lenght;
 	nod1->info.height = temp.height;
 	nod1->info.type = temp.type;
 
-	nod1->left = NULL; 
-	nod1->right = NULL; 
-	
-	if(root == NULL) 
-	{ 
-		root = nod1; 
-	} 
-	else 
-	{ 
-		nod2=root; 
-		
-	while (nod2 != NULL) 
-		{ 
-			if (temp.y < nod2->info.y) 
-			{ 
-				nod3 = nod2; 
-				nod2 = nod2->left; 
+	nod1->left = NULL;
+	nod1->right = NULL;
+
+	if(root == NULL)
+	{
+		root = nod1;
+	}
+	else
+	{
+		nod2=root;
+
+	while (nod2 != NULL)
+		{
+			if (temp.y < nod2->info.y)
+			{
+				nod3 = nod2;
+				nod2 = nod2->left;
 			}
-			else 
-			{ 
-				nod3 = nod2; 
+			else
+			{
+				nod3 = nod2;
 				nod2 = nod2->right;
 			}
 		}
-		
-		if (temp.y < nod3->info.y) 
-			nod3->left = nod1; 
-		else 
-			nod3->right = nod1; 
+
+		if (temp.y < nod3->info.y)
+			nod3->left = nod1;
+		else
+			nod3->right = nod1;
 	}
 
 	return root;
-} 
-
-NodArb *remove(MinHeap nr, NodArb **root) 
-{ 
+}
+NodArb *remove(MinHeap nr, NodArb **root)
+{
 	int temporar;
-	NodArb *tmp,*tmp1,*tmp2,*rootTemp; 
+	NodArb *tmp,*tmp1,*tmp2,*rootTemp;
 
 	rootTemp = *root;
-	tmp1 = *root; 
+	tmp1 = *root;
 	tmp = NULL;
-	
+
 	while (!((tmp1->info.y == nr.y) && (tmp1->info.x == nr.x-nr.type) && (tmp1->info.lenght == nr.lenght) && (tmp1->info.height == nr.height)))
-	{ 
-		tmp = tmp1; 
+	{
+		tmp = tmp1;
 		if (tmp1->info.y > nr.y)
-			tmp1 = tmp1->left; 
-		else 
-			tmp1 = tmp1->right; 
+			tmp1 = tmp1->left;
+		else
+			tmp1 = tmp1->right;
 	}
 
 	if (tmp1->info.y != nr.y)
@@ -121,7 +119,7 @@ NodArb *remove(MinHeap nr, NodArb **root)
 		}
 	else
 		{
-		
+
 		if( (tmp1->right==NULL) && (tmp1->left==NULL) )
 		{
 			if(tmp1==rootTemp)
@@ -136,23 +134,23 @@ NodArb *remove(MinHeap nr, NodArb **root)
 					tmp->right = NULL;
 					delete tmp1;
 				}
-				else 
+				else
 				{
-					
+
 					tmp->left = NULL;
 					delete tmp1;
 				}
 			}
 		}
 		else if (tmp1->right == NULL)
-		{ 
+		{
 			if(tmp!=NULL)
 			{
 				if(tmp->right == tmp1)
-					tmp->right = tmp1->left; 
+					tmp->right = tmp1->left;
 				else if(tmp->left == tmp1)
 					tmp->left = tmp1->left;
-				delete tmp1; 
+				delete tmp1;
 			}
 			else if (tmp1==*root)
 			{
@@ -167,9 +165,9 @@ NodArb *remove(MinHeap nr, NodArb **root)
 				delete tmp;
 
 			}
-		} 
+		}
 		else if (tmp1->left == NULL)
-		{  
+		{
 			if(tmp!=NULL)
 			{
 				if(tmp->left == tmp1)
@@ -179,8 +177,8 @@ NodArb *remove(MinHeap nr, NodArb **root)
 				}
 				else if(tmp->right ==tmp1)
 				{
-					tmp->right = tmp1->right;	
-					delete tmp1; 
+					tmp->right = tmp1->right;
+					delete tmp1;
 				}
 			}
 			else if (tmp1==rootTemp)
@@ -195,29 +193,29 @@ NodArb *remove(MinHeap nr, NodArb **root)
 				tmp1->right = tmp->right;
 				delete tmp;
 			}
-			
 
-		} 
-		else 
-		{ 
-				tmp = tmp1->right; 
+
+		}
+		else
+		{
+				tmp = tmp1->right;
 				if(tmp->left!=NULL)
 				{
-					tmp2 = tmp1; 		
-					
-					while (tmp->left != NULL) 
-					{ 
-						tmp2 = tmp; 
+					tmp2 = tmp1;
+
+					while (tmp->left != NULL)
+					{
+						tmp2 = tmp;
 						tmp = tmp->left;
-					} 
-					
+					}
+
 					tmp1->info.x = tmp->info.x;
 					tmp1->info.y = tmp->info.y;
 					tmp1->info.lenght = tmp->info.lenght;
 					tmp1->info.height = tmp->info.height;
 
-					tmp2->left = tmp->right; 
-					delete tmp; 
+					tmp2->left = tmp->right;
+					delete tmp;
 				}
 				else
 				{
@@ -234,7 +232,6 @@ NodArb *remove(MinHeap nr, NodArb **root)
 
 	return rootTemp;
 }
-
 void compare(FILE *fp,MinHeap x,NodArb *tr)
 {
 	if(tr != NULL)
@@ -255,27 +252,22 @@ void compare(FILE *fp,MinHeap x,NodArb *tr)
 			}
 	}
 }
-	
-
-void SweepLine()
+int SweepLine(char* fileToRead, char* fileToWrite)
 {
 	clock_t start,stop;
-	start = clock();
+	//start = clock();
 
-	priority_queue< MinHeap, vector<MinHeap>, greater<MinHeap> > q; 	
-	NodArb *root = NULL;	
+	priority_queue< MinHeap, vector<MinHeap>, greater<MinHeap> > q;
+	NodArb *root = NULL;
 
 	FILE *fp;
 	MinHeap temp;
-		
 
-	
-	fp = fopen ("figuri.txt", "r");
+
+
+	fp = fopen (fileToRead, "r");
 	if( fp == NULL )
-	{
-      printf("Error while opening the file.\n");
-      exit(0);
-	}
+		return 0;
 
 
 	while( ( fscanf(fp, "%d %d %d %d", &temp.x,&temp.y,&temp.lenght,&temp.height) ) != EOF )
@@ -286,18 +278,19 @@ void SweepLine()
 		temp.x = temp.x + temp.lenght;
 		temp.type = temp.lenght;
 		q.push(temp);
-		
+
 	}
 
 	fclose(fp);
-	
-	fp = fopen ("file.txt", "w+");
 
+	fp = fopen (fileToWrite, "w+");
+
+	start = clock();
 	while(q.size() != 0)
 	{
 		temp = q.top();
 		q.pop();
-		
+
 		if(temp.type == 0)
 		{
 			root = add(temp,root);
@@ -306,40 +299,37 @@ void SweepLine()
 		{
 			compare(fp,temp,root);
 			root = remove(temp,&root);
-			
+
 		}
 	}
 
 
 	//system("pause");
 	stop = clock();
-	fprintf(fp,"\nTime: %f",(double)(stop - start) );
-	
+	fprintf(fp,"\nTime: %f",((double)(stop - start))/CLOCKS_PER_SEC );
+	printf("\nTime Sweep: %f",((double)(stop - start))/CLOCKS_PER_SEC );
+
 	fclose(fp);
+	return 1;
 }
-
-
-void BruteForce()
+int BruteForce(char* fileToRead, char* fileToWrite)
 {
 	clock_t start,stop;
 	start = clock();
 	int n=0;
-	struct data brute[1000];
+	struct data brute[N];
 	FILE *fp1;
 
-	fp1 = fopen ("figuri.txt","r");
+	fp1 = fopen (fileToRead,"r");
 	if(fp1 ==NULL)
-	{
-		printf("Error while opening the file\n");
-		exit(0);
-	}
+		return 0;
 
 	while( ( fscanf(fp1,"%d %d %d %d",&brute[n].x,&brute[n].y,&brute[n].lenght,&brute[n].height)) != EOF)
 		n++;
 
 	fclose(fp1);
 
-	fp1 = fopen ("file2.txt","w+");
+	fp1 = fopen (fileToWrite,"w+");
 	for(int i=0;i<n-1;i++)
 		for(int j=i+1;j<n;j++)
 		{
@@ -350,6 +340,8 @@ void BruteForce()
 		}
 
 		stop = clock();
-		fprintf(fp1,"\nTime: %f",(double)(stop - start));
+		fprintf(fp1,"\nTime: %f",((double)(stop - start))/CLOCKS_PER_SEC);
+		printf("\nTime Brute: %f",((double)(stop - start))/CLOCKS_PER_SEC);
 		fclose(fp1);
+		return 1;
 }
