0ef9b4c1c383217d59491a930340ef12ce1205a1
MihaiVoinicu <mihai.voinicu@yahoo.com>
2014-04-25 16:53:48 +0300

    Create Prim.h

---
+++ Graphs/MSTs/Prim.h
@@ -0,0 +1,21 @@
+#ifndef _PRIM_H_
+#define _PRIM_H_
+
+#include <stdio.h>
+#include <iostream>
+
+#define VMAX 5000
+
+void loadData(int a[50][50], int Q[50], int H[50], int &n, int &r);
+
+int findEdge(int a[50][50], int Q[50], int n);
+
+void updateQ(int a[50][50], int Q[50], int n, int j);
+
+void displayMST(int H[50], int n);
+
+int prim(int a[50][50], int Q[50], int H[50], int &n, int &r);
+
+void init(int a[50][50], int Q[50], int H[50], int n);
+
+#endif
