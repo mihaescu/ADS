d8efe972b53f5cdd486e2ac8e5005c652048cf4c
SamirPrejbeanu <dvnhill@ymail.com>
2014-04-29 15:25:52 +0300

    Create Includes.h

---
+++ Data-Compression/Core/Includes.h
@@ -0,0 +1,24 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+#include <queue>
+
+#define MAX_LEN 1024
+#define MAX_NODES 512
+#define MAX_SYMBS 128
+#define MAX_CODE_LEN 32
+
+#define QueueFirst 1
+#define QueueLast QueueSize
+#define ROOT Tree[HuffmanTreeSize-1]
+#define TreeTop Queue[1]
+
+using namespace std;
+
+#endif
