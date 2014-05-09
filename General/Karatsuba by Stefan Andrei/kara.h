0a81ed9af84e642c0c0860384196ee5ab6f647b0
Stefan Andrei <stefan.andrei1992@yahoo.com>
2014-05-09 14:54:04 +0300

    Update kara.h

--- General/Karatsuba by Stefan Andrei/kara.h
+++ General/Karatsuba by Stefan Andrei/kara.h
@@ -19,4 +19,6 @@ void Produs_Clasic(char *Number1,char *Number2,char *produs,int *length_Number1,
 void karatsuba(char *Number1,char *Number2,int *length_Number1,int *length_Number2,char *produs);
 int powi(int number, int exponent);
 void sum(char *Number1_left,char *Number1_right,char *Number1_sum,int *length_Number1_left,int *length_Number1_rigth);
+void dif(char *U,char *x3,char *x2,char *x1,int *length_U);
 #endif
+
