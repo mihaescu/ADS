000c4c9567a4a1bbf16d59e398d2d7a3136360c3
Stefan Andrei <stefan.andrei1992@yahoo.com>
2014-05-17 12:54:53 +0300

    Update main.cpp

--- General/Karatsuba by Stefan Andrei/main.cpp
+++ General/Karatsuba by Stefan Andrei/main.cpp
@@ -4,24 +4,28 @@
 
 int main(){
 
-	char a[NMAX],b[NMAX],produs[2*DIMMAX];
-	int lga,lgb,lgprodus;
-	// trebuie citire din fisier!!!
-	//trebuie rezultatul in fisier!
+	char a[NMAX],b[NMAX],result[NMAX];
+	int lga,lgb,lgresult;
+	
+
 	cout<<"The first nr is: ";
-	getNum(a,&lga);
-	cout<<endl;
+	getOperand(a,&lga);
+	printf("\n");
 	cout<<"The second nr is: ";
-	getNum(b,&lgb);
-	cout<<endl;
-	
-	Produs_Clasic(a,b,produs,&lga,&lgb);
-	cout<<endl;
-	cout<<"karatsuba: "<<endl;
-	karatsuba(a,b,&lga,&lgb,produs);
-	cout<<endl;
-	//sum(a,b,&lg);
+	getOperand(b,&lgb);
+	printf("\n");
+        //trebuie citire din fisier
+        //trebuie afiasare in fisier
+	/*FILE *fp = fopen("input.txt", "r");
+	loadNumbers(fp,a,b);*/
+
+	classicProduct(a,b,result,&lga,&lgb);
+	printf("\n");
+	printf("karatsuba: ");
+	printf("\n");
+	karatsuba(a,b,&lga,&lgb,result);
+	printf("\n");
 	system("PAUSE");
-	
+
 
 }
