cfdc44dae8181b04a1e5faf47dcb347707aa559e
Stefan Andrei <stefan.andrei1992@yahoo.com>
2014-05-17 12:53:48 +0300

    Update kara.h

--- General/Karatsuba by Stefan Andrei/kara.h
+++ General/Karatsuba by Stefan Andrei/kara.h
@@ -1,3 +1,27 @@
+/*
+
+12345 = 12 · 1000 + 345  ::::: leftN1=12;rightN1=345
+6789 = 6 · 1000 + 789    ::::: leftN1=6 ;rightN1=789
+
+x1 = 12 × 6 = 72
+x2 = 345 × 789 = 272205
+sumN1 = 12 + 345
+sumN2 = 6  + 789
+U = sumN1 * sumN2
+W = U - x2 
+x3 = W - x1
+x3 = (12 + 345) × (6 + 789) − x2 − x1 = 357 × 795 − 272205 − 72 = 283815 − 272205 - 72 = 11538
+                U           - x2
+				       W         - x1
+result = 72 · 1000^2 + 11538 · 1000 + 272205 = 83810205
+
+V = x1 * 1000 = 72 * 1000 = 72000
+M = V * 1000  = 72000 * 1000 = 72000000
+K = x3 * 1000 = 11538 * 1000 = 11538000
+A = M + K  = 72000000 + 11538000 = 83538000
+B = A + x2 = 83538000 + 272205	 = 83810205
+*/          
+
 #ifndef _kara_H_
 #define _kara_H_
 
@@ -15,16 +39,19 @@
 
 using namespace std;
 
-loadNumbers(File *input, char *A, char *B);
-//void getOperand(char *operand,int *length);
+void getOperand(char *N1,int *lengthN1);
 void karatsuba(char *N1,char *N2,int *lengthN1,int *lengthN2,char *result);
 
-/*utils*/
+
+//void loadNumbers(FILE* fp, char *A, char *B);//,int *lengthN1,int *lengthN2);
+
+
+/*UTILS*/
+
 int powi(int number, int exponent);
-void sum(char *leftN1,char *rightN1,char *sumN1,int *lengthLeftN1,int *lengthRightN1);
-void dif(char *U,char *x3,char *x2,char *x1,int *length_U);
-
-
 void classicProduct(char *N1,char *N2,char *result,int *lengthN1,int *lengthN2);
-#endif
+void sum(char *leftN1,char *rightN1,char *sumN1,int *lengthLeftN1,int *lengthRightN1);
+void dif(char *U,char *x3,char *x2,int *length_U);
+//void split_Numbers(char *N1,char *N2,int *lengthN1,int *lengthN2);
 
+#endif
