38ec4e5ba3b684d6e626c9d58ef90986dc7d847f
MihaiVoinicu <mihai.voinicu@yahoo.com>
2014-05-26 12:22:49 +0300

    Update main.cpp

--- Graphs/MSTs/Prim and Kruskal by Mihai Voincu/main.cpp
+++ Graphs/MSTs/Prim and Kruskal by Mihai Voincu/main.cpp
@@ -1,99 +1,38 @@
 #include "Prim.h"
 #include "Kruskall.h"
+#include "Graph.h"
 
 int main(){
-
-	int a[50][50], Q[50], H[50], n = 0, r = 0, k=0;			//----|  Data for
-	int i = 0, x, y, z, f1, f2;								//----|		  Prim's Alg
-
-	int p, m, L[20];										//----|  Data for
-	muchie u[20];											//----|		  Kruskall's Alg
-
-	char go = 'a';
-
-	struct g{
-		int a, b, c;
-	}v[50];
+//-----------------------------------------Loading and Printing the Graph------------------------------
+	graph *G;
 
 	FILE* f;
-	f = fopen("cost.txt", "r");//doar aici se deschide
+	f = fopen("cost.txt", "r");
+	G = loadGraph(f);
 
-	//trebuie functie de incarcare date implementata in core!
-	fscanf(f, "%d", &f1);
-	fscanf(f, "%d", &f2);
+	printf("\n Edges and Costs: \n");
 
-	while(!feof(f)){
-
-			i++;
-			fscanf(f, "%d %d %d", &x,&y,&z);
-
-			v[i].a = x;
-			v[i].b = y;
-			v[i].c = z;
-		
-		}
-
-	f1=i;
-
-	k = prim(a, Q, H, n, r);//OK!
-
-	while(go != '0'){
-
-		printf("\n ||--Minimum Spanning Trees--|| \n");
-
-		printf("\n 1.Display Graph \n\n 2.Prim's MST \n\n 3.Kruskall's MST \n\n 0.Exit \n ");
-		
-		go = getch();					
-
-		system("cls");
-
-		switch(go){
-
-		case '1':
-
-			printf("\n Edges and Costs: \n");
-
-			for(i = 1; i <= f1; i++){
-				printf("[%d <-> %d] --- cost = %d \n", v[i].a, v[i].b, v[i].c);
-			}
-
-			break;
-
-		case '2':
-
-			printf("\n Prim: \n");
-
-			displayMST(H, n);
-
-			printf("\n MST cost = %d \n", k);
-
-			break;
-
-		case '3':
-
-			printf("\n Kruskall: \n");
-
-			krusk(n, m, L, u);
-
-			break;
-
-		case '0':
-
-			break;
-
-		default:
-
-			printf("\n Wrong choice! \n");
-
-			break;
-
-		}
-
+	for(int i = 0; i < G->noOfEdges; i++){
+		printf("[%d <-> %d] --- cost = %d \n", G->pEdge[i].source , G->pEdge[i].destination, G->pEdge[i].weight);
 	}
+//------------------------------------------------END---------------------------------------------------
+//-----------------------------------------------PRIM---------------------------------------------------
+	int V[50][50], Q[50], H[50], n = 0, r = 0, k=0;			//----|  Data for Prim's Alg
 
+	k = prim(V, Q, H, n, r, G);
+
+	printf("\n Prim: \n");
+
+	displayMST(H, n);
+
+	printf("\n MST cost = %d \n", k);
+//-----------------------------------------------END------------------------------------------------------
+//---------------------------------------------Kruskall---------------------------------------------------
+
+	int p, m, L[20];										//----|  Data for Kruskall's Alg
+
+	krusk(n, m, L, G);
+//-----------------------------------------------END-----------------------------------------------------
 	system("pause");
 
-	fclose(f);
-
-	return 0;
 }
