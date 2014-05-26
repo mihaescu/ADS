70e3e1567d9d769ce414fc30136edb2aa3cf762e
MihaiVoinicu <mihai.voinicu@yahoo.com>
2014-05-26 12:17:58 +0300

    Update Kruskall.cpp

--- Graphs/MSTs/Prim and Kruskal by Mihai Voincu/Kruskall.cpp
+++ Graphs/MSTs/Prim and Kruskal by Mihai Voincu/Kruskall.cpp
@@ -1,55 +1,28 @@
 #include "Kruskall.h"
+#include "Graph.h"
 
-void readData(int &n, int &m, muchie u[20]){
-
-	FILE* f = fopen("cost.txt", "rt");//trebuie pus in main!.. si transmis ca parametru!
-
-	int use;
-
-	if(f != NULL){
-
-		fscanf(f, "%d", &n);
-		fscanf(f, "%d", &use);
-
-		int i = 0;
-
-		while(!feof(f)){
-
-			i++;
-
-			fscanf(f, "%d %d %d", &u[i].x, &u[i].y, &u[i].c);
-
-		}
-
-		m = i;
-
-		fclose(f);
-
-	}
-
-	for(int i = 0; i < m; i++)
-
-		for(int j = i + 1; j < m; j++)
-
-			if(u[i].c > u[j].c){
-
-				muchie aux;
-
-				aux = u[i];
-				u[i] = u[j];
-				u[j] = aux;
-
-			}
-
-}
-
-void krusk(int &n, int &m, int L[20], muchie u[20]){
-
-	int i = 1, k = 0, ct = 0;
+void krusk(int &n, int &m, int L[20], graph *G){
+//--------------------------------------Preparation----------------------------------
+	int i = 1, j, k = 0, ct = 0;
 	int x, y;
 
-	readData(n, m, u);
+	n = G->noOfVertices;
+	
+	for(i = 0; i < m; i++)
 
+		for(j = i + 1; j < m; j++)
+
+			if(G->pEdge[i].weight > G->pEdge[j].weight){
+
+				edge aux;
+
+				aux = G->pEdge[i];
+				G->pEdge[i] = G->pEdge[j];
+				G->pEdge[j] = aux;
+
+			}
+//--------------------------------------END----------------------------------------
+//-------------------------------------Kruskall------------------------------------
 	for(int i = 0 ; i < n ; i++)
 
 		L[i] = i;
@@ -58,16 +31,16 @@ void krusk(int &n, int &m, int L[20], muchie u[20]){
 
 	while(k < n-1){
 
-		if(L[u[i].x] != L[u[i].y]){
+		if(L[G->pEdge[i].source] != L[G->pEdge[i].destination]){
 
 			k++;
 
-			ct = ct + u[i].c;
+			ct = ct + G->pEdge[i].weight;
 
-			printf("[%d <-> %d]\n", u[i].x, u[i].y);
+			printf("[%d <-> %d]\n", G->pEdge[i].source, G->pEdge[i].destination);
 
-			x = L[u[i].y];					//unite the MSTs through edge [x,y]
-			y = L[u[i].x];
+			x = L[G->pEdge[i].destination];					//unite the MSTs through edge [x,y]
+			y = L[G->pEdge[i].source];
 
 			for(int j = 0; j < n; j++)
 
@@ -82,5 +55,5 @@ void krusk(int &n, int &m, int L[20], muchie u[20]){
 	}
 
 	printf("\n Total cost = %d \n",ct);
-
+//--------------------------------------------------End------------------------------------------------
 }
