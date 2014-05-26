93738829264313a30279e7a3673f2cab2ea50c92
mihaescu <mihaescu@software.ucv.ro>
2014-05-26 11:27:47 +0300

    comments

--- SubstringSearch/Rabin-Karp by Veronica Dan/Rabin-Karp.h
+++ SubstringSearch/Rabin-Karp by Veronica Dan/Rabin-Karp.h
@@ -5,6 +5,7 @@
 #include<string.h>
 
 #define MAX 10000 //max dimension for the input file
+#define q 101//large prime ...~NM^2, N=text length, M=pattern length
 
 int RabinKarp(struct Text &pattern, struct Text &txt, int q);
 long modularHashing(struct Text &txt, int lenght, int q);
