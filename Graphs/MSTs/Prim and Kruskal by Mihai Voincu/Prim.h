2aee6059db952fc1d80c958b72d3898e3aab6002
MihaiVoinicu <mihai.voinicu@yahoo.com>
2014-05-26 12:22:05 +0300

    Update Prim.h

--- Graphs/MSTs/Prim and Kruskal by Mihai Voincu/Prim.h
+++ Graphs/MSTs/Prim and Kruskal by Mihai Voincu/Prim.h
@@ -4,21 +4,18 @@
 #include <stdio.h>
 #include <iostream>
 #include <conio.h>
+#include "Graph.h"
 
 #define VMAX 5000
 
-//trebuie in core
-void loadData(int a[50][50], int Q[50], int H[50], int &n, int &r);
-
-//trebuie vazut!
 int findEdge(int a[50][50], int Q[50], int n);
+
 void updateQ(int a[50][50], int Q[50], int n, int j);
 
 void displayMST(int H[50], int n);
 
-int prim(int a[50][50], int Q[50], int H[50], int &n, int &r);
+int prim(int a[50][50], int Q[50], int H[50], int &n, int &r, graph *G);
 
-//in core
 void init(int a[50][50], int Q[50], int H[50], int n);
 
 #endif
