47901cd289138fa044217a6886c211fa806a9aac
Vladutu Georgian <vladutu_georgian_4d@yahoo.com>
2014-10-30 22:42:44 +0200

    Create main.cpp

---
+++ Graphs/Distances/MonotonicShortestPath by Vladutu Georgian/main.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"msp.h"
+
+
+int main(){
+
+	graph *g;
+	int source;
+	FILE *f = fopen("input.txt","r");
+	g = readGraph(f);
+	printf("Insert source node:\n");
+	scanf("%d", &source);
+	int *dist, *pred;
+	dist = createDist(g);
+	pred = createPred(g);
+	MSP(g, source,dist,pred);
+	findPath(g, dist, pred, source);
+
+	
+
+	system("PAUSE");
+	return 0;
+}
