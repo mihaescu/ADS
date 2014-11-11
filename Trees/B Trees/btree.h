f5f0181650e69459f82b62f6a02cf42cff39a7b4
mihaescu <mihaescu@software.ucv.ro>
2014-11-11 12:42:38 +0200

    review BTree

--- Trees/B Trees/btree.h
+++ Trees/B Trees/btree.h
@@ -7,6 +7,8 @@
 #include<string.h>
 #define MAX 10001
 
+/** STRUCTURE */
+
 typedef struct NodeB_
 {
     int nr;
@@ -14,18 +16,26 @@ typedef struct NodeB_
     struct NodeB_ *pNodes[m+1];
 }NodeB;
 
+/** DATA MANAGERS*/
+void Generate(FILE *f1,int n, int a, int b);
+void LoadData(FILE *f1,int n,int vect[MAX]);
+
+/** MAIN FUNCTIONS*/
 NodeB *Insert(NodeB *root, int x);
+void Search(NodeB *r, int x);
+void DeleteKey(NodeB **node, int key);
+void DisplayTree(NodeB *node, int lever);
+
+/** UTIL FUNCTIONS*/
 void SplitPage(NodeB *page, int x, int *ind, int *urc, int *h, NodeB **adr, NodeB **purc);
 void PlaceInPage(NodeB *page, int k, int x, int *ind, int *urc, int *h, NodeB **adr, NodeB **purc);
 void InsertInNonFullLeaf(NodeB *r, int k, int x, int *ind, int *urc, int *h, NodeB **adr, NodeB **purc);
 void CopyPage(NodeB *destination, NodeB *source, int destIndex, int sourceIndex, int n);
-void Search(NodeB *r, int x);
-void DisplayTree(NodeB *node, int lever);
-void DeleteKey(NodeB **node, int key);
 int FindMinimum(NodeB *node);
 
-void Generate(FILE *f1,int n, int a, int b);
-void LoadData(FILE *f1,int n,int vect[MAX]);
+/**TEST FUNCTIONS*/
+
+
 
 
 #endif
