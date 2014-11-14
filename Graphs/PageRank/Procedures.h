baac28ec9d07ca14b45fe250af148b9e3bd582ff
Adrian-Ghita <adryghi@yahoo.com>
2014-11-14 14:04:43 +0200

    Update Procedures.h

--- Graphs/PageRank/Procedures.h
+++ Graphs/PageRank/Procedures.h
@@ -1,22 +1,21 @@
 #pragma once
 
-#include <iostream>
-#include <fstream>
-#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
+#include <time.h>
 #define beta 0.85  // damping factor (best between 0.8 and 0.9)
 #define eps 10000  // the greater the eps, the greater the precision of the pagerank score
 
-using namespace std;
+template <class T>
+void deallocMatrix (T**, int ); 
 
-//ifstream f;
-
-void allocMatrix (double**, int, int);
-void deallocMatrix (double**, int); 
+void graphGenerator	(char*);
+double **loadGraph (FILE* fin, int&);
 void displayMatrix (double**, int, int);
-void readWebGraph (double**, int, int);
-void createMatrix_M (double**, int);
-void createMatrix_B (double**, int);
-void matrixSum (double**, double**, int);
-void createVector_R (double**, int);
-void matrixMultiply (double**, double**, double**, int);
+double **createMatrix_M (double**, int);
+double **createMatrix_B (int);
+double **matrixSum (double**, double**, int);
+double **createVector_R (int);
+double **matrixMultiply (double**, double**, double**, int);
+void powerIteration (double**, int);
