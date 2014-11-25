13bdc83d2c4a6745d05c795faecceb4c65d7e48e
Adrian-Ghita <adryghi@yahoo.com>
2014-11-25 13:19:18 +0200

    Update 25.11.2014

--- Graphs/PageRank/Procedures.h
+++ Graphs/PageRank/Procedures.h
@@ -7,15 +7,42 @@
 #define beta 0.85  // damping factor (best between 0.8 and 0.9)
 #define eps 10000  // the greater the eps, the greater the precision of the pagerank score
 
-template <class T>
-void deallocMatrix (T**, int ); 
+/*---------- CORE FUNCTIONS ---------- */
 
-void graphGenerator	(char*);
+/* create matrix M with *beta* probability of random teleporting, based on
+the adjaceny matrix sent as parameter*/
+double **createMatrix_M (double**, const int&);
+
+/* create matrix B with *beta* probability of random teleporting */
+double **createMatrix_B (const int&);
+
+/* create the column-vector (eigenvector) R */
+double **createVector_R (const int&);
+
+/* repeatedly multiply matrix A with vector R, until R = A x R */
+void powerIteration (double**, const int&);
+
+
+/*---------- UTILITY FUNCTIONS ----------*/
+
+/* deallocate memory for a matrix */
+template <class T>
+void deallocMatrix (T**, int );   
+
+/* generate a random digraph in a file (file name given as parameter)*/
+void graphGenerator	(char*);		
+
+/* load graph from a given file, the integer holding the number of nodes */
 double **loadGraph (FILE* fin, int&);
-void displayMatrix (double**, int, int);
-double **createMatrix_M (double**, int);
-double **createMatrix_B (int);
-double **matrixSum (double**, double**, int);
-double **createVector_R (int);
-double **matrixMultiply (double**, double**, double**, int);
-void powerIteration (double**, int);
+
+/* display graph in console */
+void displayGraph (FILE* fin, char*);
+
+/* display matrix in console */
+void displayMatrix (double**, const int&, const int&);
+
+/* compute the sum of 2 matrices */
+double **matrixSum (double**, double**, const int&);
+
+/* compute the product of 2 matrices */
+double **matrixMultiply (double**, double**, double**, const int&);
