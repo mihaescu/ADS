ef1e9c9a9c649d4e414de7f4deb600c33727f9f9
Marcu Marian <rcmarcu@gmail.com>
2014-11-28 12:44:30 +0200

    v0.001

--- Data-Compression/Alpha GUI/Alpha GUI/MainWindow.h
+++ Data-Compression/Alpha GUI/Alpha GUI/MainWindow.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 
 namespace AlphaGUI {
 
@@ -34,6 +39,9 @@ using namespace System::Drawing;
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
+	protected: 
+	private: System::Windows::Forms::Button^  button1;
 
 	private:
 		/// <summary>
@@ -48,13 +56,30 @@ using namespace System::Drawing;
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(485, 57);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainWindow::button1_Click);
 			// 
 			// MainWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(838, 500);
+			this->Controls->Add(this->button1);
 			this->Name = L"MainWindow";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"MainWindow";
@@ -62,5 +87,21 @@ using namespace System::Drawing;
 
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) 
+			 {
+				
+				 openFileDialog1->ShowDialog();
+
+				String^ path = openFileDialog1->FileName->ToString();
+			    string path2 ;
+				for (int i=0; i < path->Length;i++)
+				{
+					path2[i] = path[i];
+				}
+				ifstream f(path2);
+				string content;
+				f>>content;
+				
+			 }
 	};
 }
