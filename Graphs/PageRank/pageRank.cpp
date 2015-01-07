ab7aae0d479fb58cdf1fed5b3a5bdbd4be5c1a79
Adrian-Ghita <adryghi@yahoo.com>
2015-01-07 16:51:32 +0200

    Update pageRank.cpp

--- Graphs/PageRank/pageRank.cpp
+++ Graphs/PageRank/pageRank.cpp
@@ -7,11 +7,11 @@ int main()
 
 	do
 	{
-		printf("If you want to generate a new test file AND display it, press 1;\n");
-		printf("If you want to generate a new test file WITHOUT displaying it, press 2;\n\n");
-		printf("If you want to load an existing test file AND display it, press 3;\n");
-		printf("If you want to load an existing test file WITHOUT displaying it, press 4;\n\n");
-		printf("If you want to exit the program, press 5.\n");
+		printf ("If you want to generate a new test file AND display it, press 1;\n");
+		printf ("If you want to generate a new test file WITHOUT displaying it, press 2;\n\n");
+		printf ("If you want to load an existing test file AND display it, press 3;\n");
+		printf ("If you want to load an existing test file WITHOUT displaying it, press 4;\n\n");
+		printf ("If you want to exit the program, press 5.\n");
 
 		printf ("\nChoose an option: ");
 		scanf (" %c", &c);
@@ -45,34 +45,49 @@ int main()
 		}
 
 		else if (c == '5')
+		{
 			exit(1);
+		}
+
 		else
 		{
-			printf("\nThe key you pressed is not a valid option. Please press 1, 2, 3, 4 or 5.\n");
+			printf ("\nThe key you pressed is not a valid option. Please press 1, 2, 3, 4 or 5.\n\n");
 		}
-	} while ((c != '1') && (c != '2') && (c != '3') && (c != '4'));
 
+		if (c == '1' || c == '2' || c == '3' || c == '4')
+		{
+			fin = fopen (fileName, "r");
 
-	fin = fopen (fileName, "r");
+			if (fin == NULL) 
+			{
+				perror ("\nError opening file\n");
+			}
+			else
+			{
+				int nodes;
+				double beta;
 
-	if (fin == NULL) 
-		perror ("Error opening file");
-	else
-	{
-		int nodes;
+				printf ("\nDefine the damping factor (best between 0.8 and 0.9): ");
+				scanf ("%lf", &beta);
 
-		double **M = loadGraph (fin, nodes);
-		M = createMatrix_M (M, nodes);
+				double **M = loadGraph (fin, nodes);
+				M = createMatrix_M (M, nodes, beta);
 
-		double **B = createMatrix_B (nodes);
-		M = matrixSum (M, B, nodes);
-		deallocMatrix <double> (B, nodes);
+				double **B = createMatrix_B (nodes, beta);
+				M = matrixSum (M, B, nodes);
+				deallocMatrix <double> (B, nodes);
 
-		/*printf("\n Matrix A = M + B \n");
-		displayMatrix (M, nodes, nodes);*/
+				//
+				printf("\n Matrix A = M + B \n");
+				displayMatrix (M, nodes, nodes);
+				//
 
-		powerIteration (M, nodes);
-	}
+				printf ("\n Damping factor: %.3f \n", beta);
+				powerIteration (M, nodes);
+			}
+		}
+
+	} while (true);
 
 	return 0;
 }
