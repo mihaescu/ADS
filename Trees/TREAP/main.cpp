738c96768d8acd6d0bf25bc86bb07cdeb42e4cff
Totolin <Totolin@users.noreply.github.com>
2015-03-09 15:43:06 +0200

    Update main.cpp

--- Trees/TREAP/main.cpp
+++ Trees/TREAP/main.cpp
@@ -1,123 +1,44 @@
-//#include<iostream>
-//using namespace std;
-#include "treaps.h"
+#include"Treap.h"
+#include<stdio.h>
+#include<stdlib.h>
 #include<conio.h>
-
-typedef int dir;
-NodeTreap root;
-NodeTreap tLess,tLarge;
-NodeTreap extraNode;
-
-
-
-
 int main()
 {
-	//srand(time(NULL));
+	Treap* treap;
+	init(treap);
 
-	int opt;
-	int el,n;
 
-	float p;
-
-	root = NULL;
-	tLess = NULL;
-	tLarge = NULL;
-
+	char opt;
 	do
 	{
-		printf("\nMenu");
-		printf("\n1. Create/Insert");
-		printf("\n2. Search");
-		printf("\n3. Delete");
-		printf("\n4. Print tree");
-		printf("\n5. Split");
-		printf("\n6. Merge");
-		printf("\n0. Exit\n");
-		scanf("%d",&opt);
-		printf("\n\n");
-
+		system("CLS");
+		printf("1.Insert\n2.Delete\n3.Display\n0.Exit\n");
+		opt = _getch();
+		int key = 0, priority = 0;
 		switch (opt)
 		{
-		case 1:
-			printf("\nEnter key and priority : ");
-			scanf("%d%f",&el,&p);
-			insert(&root,el,p);
-			displayTreap(root,0);			
+		case '1':
+			printf("Key:");
+			scanf("%d", &key);
+			printf("Priority:");
+			scanf("%d", &priority);
+			insert(treap, key, priority);
 			break;
-		case 2:
-			printf("\nEnter searched key : ");
-			scanf("%d",&el);
-
-			if (member(&root,el))			
-				printf("The key is in the tree\n");			
-			else			
-				printf("The key is not in the tree\n");			
-			break;		
-		case 3:
-			printf("\nEnter the key to be deleted : ");
-			scanf("%d",&el);
-			deleteTreap(&root,el);
-			displayTreap(root,0);
+		case '2':
+			printf("Key:");
+			scanf("%d", &key);
+			erase(treap, key);
 			break;
-		case 4:
-			if (root)
-			{
-				printf("\nThe tree is : \n");
-				displayTreap(root,0);
-				printf("\n The root is: %d\n",root->element);
-			} 
-			else
-			{
-				printf("\ntLess is :\n");
-
-				displayTreap(tLess,0);
-
-				printf("\ntLarge is :\n");
-
-				displayTreap(tLarge,0);
-			}			
-			break;
-		case 5: printf("\n Enter the key:");
-			     scanf("%d",&el);
-				 splitTreap(&root,el);
-				 displayTreap(root,0);
-		case 6:
-			printf("Number of elements :");
-			     scanf("%d",&n);
-				 while(n!=0)
-				 {
-			     printf("\n Enter key and priority:");
-			     scanf("%d %f",&el,&p);
-			     insert(&tLess,el,p);
-			     displayTreap(tLess,0);
-				 n--;
-				 }
-				 printf("Number of elements :");
-			     scanf("%d",&n);
-				 while(n!=0)
-				 {
-				 printf("\n Enter key and priority:");
-			     scanf("%d %f",&el,&p);
-				 insert(&tLarge,el,p);
-				  displayTreap(tLarge,0);
-				 n--;
-				 }
-	
-				 printf("Element: ");
-				 scanf("%d",&el);
-				 insert(&extraNode,el,p);
-			mergeTreaps(&root,&tLess,&tLarge);			
-			tLess = NULL;
-			tLarge = NULL;
-			break;
-		default:
+		case '3':
+			displayTreap(treap, 1);
+			system("PAUSE");
 			break;
 		}
+	} while (opt != '0');
 
-		if (opt != 0)
-			getch();
-	}while(opt != 0);
 
+
+
+	system("PAUSE");
 	return 0;
 }
