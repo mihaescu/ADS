11a5aba069c3d5734eb73b15cbda57fe2d8c4c6c
Cătălin Modan <modan.catalin@outlook.com>
2017-10-31 16:08:21 +0200

    The first version of the project
    Added code for basic operations in a BST as well as few extra functions.

---
+++ Projects-2017-2018/BST-isOrderedByCatalinModan/prototypes.h
@@ -0,0 +1,33 @@
+#ifndef PROTOTYPES_H_INCLUDED
+#define PROTOTYPES_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include <time.h>
+#define G_plaja_valori 1000
+
+
+
+typedef struct _Node {
+        int data;
+        struct _Node *left;
+        struct _Node *right;
+}
+Node;
+
+void Display( Node *root, int nivel );
+Node *Add_Node( Node *root, int data );
+void preorder( Node *root );
+void inorder( Node *root );
+void postorder( Node *root );
+Node *FindMin( Node *root );
+Node *FindMax( Node *root );
+Node *Delete( Node *root, int data );
+int getMin( Node *node );
+int getMax( Node *node );
+int randomNumberGenerator();
+
+
+
+#endif // PROTOTYPES_H_INCLUDED
