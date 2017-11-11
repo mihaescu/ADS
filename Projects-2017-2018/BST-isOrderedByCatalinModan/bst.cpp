39a239e8cc730f46b5ec1cc9f31c75980b13a123
Cătălin Modan <modan.catalin@outlook.com>
2017-11-11 13:47:02 +0200

    6th edition released
    -improved file names
    -added descriptions of prototypes
    -ordered the prototypes

---
+++ Projects-2017-2018/BST-isOrderedByCatalinModan/bst.cpp
@@ -0,0 +1,225 @@
+#include "bst.h"
+
+//The function prints the BST like a tree
+void Display( Node *node, int level )
+{
+    //Setting up an iterator
+    int i;
+
+    //Checking if the node is NULL
+    if ( node != 0 ) {
+        //If not, go right and print next level
+        Display( node->right, level + 1 );
+
+        //Print as many spaces as the level (the node has no spaces)
+        for ( i = 0; i <= level; i++ ) {
+            printf( "      " );
+        }
+
+        //Print the values of the nodes
+        printf( "%d\n", node->data );
+
+        //Go left and print the next level
+        Display( node->left, level + 1 );
+    }
+}
+
+//The function prints all the values in the BST likewise: node, left, right
+void preorder( Node *node )
+{
+    //Check if the node is NULL
+    if ( node != NULL ) {
+
+        //If not, print its value
+        printf( "\n%d", node->data );
+
+        //Recur left
+        preorder( node->left );
+        //Recur right
+        preorder( node->right );
+    }
+}
+
+//The function prints all the values in the BST likewise: left, node, right
+void inorder( Node *node )
+{
+    ////Check if the node is NULL
+    if ( node != NULL ) {
+        //If not recur left
+        inorder( node->left );
+        //Print its value
+        printf( "\n%d", node->data );
+        //Recur right
+        inorder( node->right );
+    }
+}
+
+//The function prints all the values in the BST likewise: right, left, node
+void postorder( Node *node )
+{
+    //Check if the node is NULL
+    if ( node != NULL ) {
+        //Recur left
+        postorder( node->left );
+        //Recur right
+        postorder( node->right );
+        //Print its data
+        printf( "\n%d", node->data );
+    }
+}
+
+//The function is used later to determine the successor value of a node
+Node *FindMin( Node *node )
+{
+    //Go to the last value of the left sub-tree
+    while ( node->left != NULL ) {
+        node = node->left;
+    }
+
+    //Return the last found node
+    return node;
+}
+
+//This function is used to delete a node from the BST
+Node *Delete( Node *node, int data )
+{
+    //Find the position of the node to be deleted
+    //Check if the node is NULL
+    if ( node == NULL ) {
+        //If true return it
+        return node;
+    } else if ( data < node->data ) {
+        //Else recur left (the value is smaller than the current node's)
+        node->left = Delete( node->left, data );
+    }
+
+    else if ( data > node->data ) {
+        //Else recur right (the value is greater than the current node's)
+        node->right = Delete( node->right, data );
+    }
+
+    else {
+        //If both children are NULL, the node is a leaf. Deallocate its memory and set it to NULL
+        if ( node->left == NULL && node->right == NULL ) {
+            //Deallocating memory
+            free( node );
+            //Setting it to NULL
+            node = NULL;
+            //If the node has a child on the right (no child on the left).
+        } else if ( node->left == NULL ) {
+            //Create a temporary node and initialize it to the current working node
+            Node *temp = node;
+            //Set it to point to its right child
+            node = node->right;
+            //Deallocate memory
+            free( temp );
+            //If the node has a child on the left (no child on the right)
+        } else if ( node->right == NULL ) {
+            //Create a temporary node and initialize it to the current working node
+            Node *temp = node;
+            //Set it to point to its left child
+            node = node->left;
+            //Deallocate memory
+            free( temp );
+        }
+        //If it has two children (both right and left are non-NULL)
+        else {
+            //Create a temporary node and initialize it to the successor of the node. (the smallest value from the right sub-tree)
+            Node *temp = FindMin( node->right );
+            //Node's data is replaced by the successor's data
+            node->data = temp->data;
+            //Delete the successor node from the BST.
+            node->right = Delete( node->right, temp->data );
+        }
+    }
+
+    //Return the node
+    return node;
+}
+
+//Insert a new value in the BST
+Node *Add_Node( Node *node, int data )
+{
+    //Check if the current node is a NULL
+    if ( node == NULL ) {
+        //If yes, then allocate memory to the size of a node
+        node = ( Node * )malloc( sizeof( Node ) );
+        //Set its data to whatever value you want to insert
+        node->data = data;
+        //Set both of its children to NULL (every node is inserted as a LEAF!)
+        node->left = node->right = NULL;
+        //Else recur left (the value we want to insert is smaller than the current node's)
+    } else if ( data <= node->data ) {
+        //Recur left
+        node->left = Add_Node( node->left, data );
+        //Else recur right (the value we want to insert is greater than the current node's)
+    } else {
+        //Recur right
+        node->right = Add_Node( node->right, data );
+    }
+
+    //Return the node
+    return node;
+}
+
+//The function determines the smallest value in the BST
+int getMin( Node *node )
+{
+    //Create a temporary node and initialize it to root. (that's how me make the function call)
+    Node *node_iterator = node;
+
+    //Go the last left node
+    while ( node_iterator->left != NULL ) {
+        node_iterator = node_iterator->left;
+    }
+
+    //Return the smallest value
+    return ( node_iterator->data );
+}
+
+//The function determines the greatest value in the BST
+int getMax( Node *node )
+{
+    //Create a temporary node and initialize it to root. (that's how me make the function call)
+    Node *node_iterator = node;
+
+    //Go to the last right node
+    while ( node_iterator->right != NULL ) {
+        node_iterator = node_iterator->right;
+    }
+
+    //Return the greatest value
+    return ( node_iterator->data );
+}
+
+//A function which returns a random value
+int randomNumberGenerator()
+{
+    //Using the built-in rand function
+    return rand() % G_values_limit;
+}
+
+//The function determines if all the BST's values are bounded by 2 values
+bool checkValues( Node *node, int minimum, int maximum )
+{
+    //Get the global minimum and maximum of the BST
+    int global_minimum = getMin( node );
+    int global_maximum = getMax( node );
+
+    //Check if the BST minimum and maximum are bounded by the 2 values
+    if ( global_minimum >= minimum && global_maximum <= maximum ) {
+        //If true, proceed to check all the node's values are bounded by those 2 values
+        //Check if the current node's data is bounded and recur left and right
+        if ( node->data < global_maximum && node->data > global_minimum ) {
+            //Check the left child's data
+            return checkValues( node->left, minimum, maximum );
+            //Check the right child's data
+            return checkValues( node->right, minimum, maximum );
+        }
+        //Return true if all the values are bounded
+        return true;
+    } else {
+        //Return false otherwise
+        return false;
+    }
+}
---
+++ Projects-2017-2018/BST-isOrderedByCatalinModan/bst.h
@@ -0,0 +1,51 @@
+#ifndef PROTOTYPES_H_INCLUDED
+#define PROTOTYPES_H_INCLUDED
+
+//Libraries definitions
+#include<stdio.h>
+#include <stdlib.h>
+//Defining the maximum value for our random generator
+#define G_values_limit 1000
+
+
+
+
+
+//Structure definition
+typedef struct _Node {
+    // Structure member containing the data ( the integers)
+    int data;
+    // Structure pointers to left and right children
+    struct _Node *left;
+    struct _Node *right;
+} Node;
+
+
+//Prototypes definitions
+
+//A function to display the BST in a tree-like manner ( from root to branches )
+void Display( Node *node, int level );
+//Pre-order traversal of the BST ( it will print current node, left, right )
+void preorder( Node *node );
+//In-order traversal of the BST ( it will print left, current node,  right )
+void inorder( Node *node );
+//In-order traversal of the BST ( it will print left, right, current node, )
+void postorder( Node *node );
+//A function which determines the successor value of a node ( used when deleting the root )
+Node *FindMin( Node *node );
+//A function which deletes a node from the BST
+Node *Delete( Node *node, int data );
+//A function which adds a new node to the BST
+Node *Add_Node( Node *node, int data );
+//A function to retrieve the minimum value of the BST
+int getMin( Node *node );
+//A function to retrieve the maximum value of the BST
+int getMax( Node *node );
+//A functions which returns random numbers
+int randomNumberGenerator();
+//A function which checks if our BST is bounded by two values
+bool checkValues( Node *node, int minimum, int maximum );
+
+
+
+#endif // PROTOTYPES_H_INCLUDED
