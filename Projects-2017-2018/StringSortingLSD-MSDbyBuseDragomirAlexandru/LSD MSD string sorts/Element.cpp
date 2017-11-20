d774f77183313c0ceadd934e8709912dec1d5b6d
Islaya <26760150+Islaya@users.noreply.github.com>
2017-11-20 15:36:29 +0200

    Add files via upload

---
+++ Projects-2017-2018/StringSortingLSD-MSDbyBuseDragomirAlexandru/LSD MSD string sorts/Element.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include "Element.h"
+
+using namespace std;
+
+Element :: Element()
+{
+    this -> word = "";
+    this -> key = 0;
+}
+
+Element :: Element(string word, int key)
+{
+    this -> word = word;
+    this -> key = key;
+}
+
+Element :: ~Element()
+{
+    ///cout << "Destructor";
+}
+
+void Element :: setWord(string word)
+{
+    this -> word = word;
+}
+
+void Element :: setKey(int key)
+{
+    this -> key = key;
+}
+
+string Element :: getWord()
+{
+    return this -> word;
+}
+
+int Element :: getKey()
+{
+    return this -> key;
+}
---
+++ Projects-2017-2018/StringSortingLSD-MSDbyBuseDragomirAlexandru/LSD MSD string sorts/key_indexed_counting.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+void key_indexed_counting(vector<Element> my_array, int alphabet_size);
