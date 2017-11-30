b6ff4d77eb150af110e9d2fc89770420e7c3201f
Mălina Nețoiu <malina_netoiu@yahoo.com>
2017-11-30 15:28:42 +0200

    Update mergesort.h

--- Projects-2017-2018/IndirectSortByNetoiuMalina/mergesort.h
+++ Projects-2017-2018/IndirectSortByNetoiuMalina/mergesort.h
@@ -9,4 +9,8 @@ int index_array(int arr[100], int perm[100], int n);
 
 int rand_number(int min_val, int max_val);
 
+bool is_valid(int arr[100], int perm[100], int n);
+
+void output(int arr[100], int perm[100], int n);
+
 #endif // MERGESORT_H
