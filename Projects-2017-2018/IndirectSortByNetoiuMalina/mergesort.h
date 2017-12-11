4450cd8994321d1a1d066edf01d8ac8a4e7fb658
Mălina Nețoiu <malina_netoiu@yahoo.com>
2017-12-11 10:44:42 +0200

    Update mergesort.h

--- Projects-2017-2018/IndirectSortByNetoiuMalina/mergesort.h
+++ Projects-2017-2018/IndirectSortByNetoiuMalina/mergesort.h
@@ -1,6 +1,14 @@
 #ifndef MERGESORT_H
 #define MERGESORT_H
 
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+const int n = 100;
+
 void merges(int arr[100], int perm[100], int left, int mid, int right);
 
 void mergesort(int arr[100], int left, int right);
@@ -12,5 +20,7 @@ int rand_number(int min_val, int max_val);
 bool is_valid(int arr[100], int perm[100], int n);
 
 void output(int arr[100], int perm[100], int n);
+
+void create_file(string filename);
 
 #endif // MERGESORT_H
